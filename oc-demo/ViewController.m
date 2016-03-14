925756ce0917aaec2c9e4d07fe1cdd407ac74f58
崔明强 <cuimingqiang@yxonline.com>
2016-03-14 21:03:43 +0800

    init

--- oc-demo/ViewController.m
+++ oc-demo/ViewController.m
@@ -17,6 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *button = [[UIButton alloc] init];
+    UIButton *button1 = [[UIButton alloc] init];
+    NSDictionary *views = NSDictionaryOfVariableBindings(button,button1);
+    button.backgroundColor = [UIColor blackColor];
+    button.translatesAutoresizingMaskIntoConstraints = NO;
+    [button setTitle:@"点击" forState:UIControlStateNormal];
+    [self.view addSubview:button];
+
+    NSArray *constraints1 = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[button]-|" options:0 metrics:nil views:views];
+    NSArray *constraints2 = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-28-[button(==50)]" options:0 metrics:nil views:views];
+    [self.view addConstraints:constraints1];
+    [self.view addConstraints:constraints2];
+
+    button1.backgroundColor = [UIColor blueColor];
+    button1.translatesAutoresizingMaskIntoConstraints = NO;
+    [button1 setTitle:@"你妹" forState:UIControlStateNormal];
+    [self.view addSubview:button1];
+
+    NSArray *con1 = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[button1]-|" options:0 metrics:nil views:views];
+    NSArray *con2 = [NSLayoutConstraint constraintsWithVisualFormat:@"V:[button]-[button1(==40)]" options:0 metrics:nil views:views];
+    [self.view addConstraints:con1];
+    [self.view addConstraints:con2];
 }
 
 - (void)didReceiveMemoryWarning {
